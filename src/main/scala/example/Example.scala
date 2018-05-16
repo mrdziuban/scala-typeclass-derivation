091eae5d9b6a2298740bdeb5d11b22ea9c0f1577
Matt Dziuban <mrdziuban@gmail.com>
2018-05-16 16:21:34 -0400

    Properly reproduce diverging implicit.

--- src/main/scala/example/Example.scala
+++ src/main/scala/example/Example.scala
@@ -10,20 +10,17 @@ trait LPCsv {
 
 object Example extends LPCsv {
   implicit val csvStr: Csv[String] = new Csv[String] { def apply(a: String): List[String] = List(a) }
-  implicit val csvInt: Csv[Int] = new Csv[Int] { def apply(a: Int): List[String] = List(a.toString) }
-  implicit val csvBool: Csv[Boolean] = new Csv[Boolean] { def apply(a: Boolean): List[String] = List(a.toString) }
 
   implicit def csvSeq[A](implicit sa: Csv[A]): Csv[Seq[A]] = new Csv[Seq[A]] {
     def apply(a: Seq[A]): List[String] = a.flatMap(sa(_)).toList
   }
 
   case class Foo(s: String)
-  case class Wrapper[A](a: A)
+  case class TypeParam[A](a: A)
+  case class Wrapper(tp: TypeParam[Foo])
 
   // Deriving this results in a diverging implicit error
-  case class Test(wrapper: Seq[Wrapper[Foo]])
+  case class Test(wrapper: Seq[Wrapper])
 
   implicitly[Csv[Test]]
 }
-
-
