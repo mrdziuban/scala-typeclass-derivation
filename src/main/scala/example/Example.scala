9db5c5f972fd8b699f30693ade9c0d464b332355
Matt Dziuban <mrdziuban@gmail.com>
2018-05-16 10:30:01 -0400

    Add error in comment.

--- src/main/scala/example/Example.scala
+++ src/main/scala/example/Example.scala
@@ -4,4 +4,9 @@ import example.macros.ExampleMacro
 
 object Example {
   def go[A] = ExampleMacro.go[A]
+  /*
+  [error] src/main/scala/example/Example.scala:6:30: A is not a class
+  [error]   def go[A] = ExampleMacro.go[A]
+  [error]                              ^
+  */
 }
